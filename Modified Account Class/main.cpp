25a9f26ad042bdbc617b326cc01deb0a11fe17a1
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-10-05 22:33:40 -0700

    Initial Commit

---
+++ Modified Account Class/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Modified Account Class
+//
+//  Created by Amanpreeet Sandhu on 2024-10-05.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
